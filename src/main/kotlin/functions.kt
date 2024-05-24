b25f0020efb38f88b3f8b70c9562279e2c2e9ab5
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-05-24 14:55:37 +0200

    Scanner

---
+++ src/main/kotlin/functions.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import java.util.Scanner
+
+fun main() {
+    // Kiedy funkcja nie zwraca nic, zwraca typ Unit podobnie jak Void w Java
+
+    readln() //czyta linie
+    val scanner = Scanner(System.`in`); //możemy sięgać do bibliotek javy normalnie
+    Scanner("sdsds") // możemy wskazać źródło sami
+    scanner.useDelimiter("_") // mozemy ustawiac wlasny delimiter
+    scanner.hasNext() // sprawdza czy są dane do odczytu
+}
