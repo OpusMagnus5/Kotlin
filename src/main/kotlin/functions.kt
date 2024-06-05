f542bf3dc720567feb1ac20e36c513ee77d22672
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-06-05 18:39:45 +0200

    functions

--- src/main/kotlin/functions.kt
+++ src/main/kotlin/functions.kt
@@ -12,4 +12,14 @@ fun main() {
     scanner.hasNext() // sprawdza czy są dane do odczytu
 
     val (a, b, c, d) = readln().split(",") //wczytanie i rozdzielenie inputów, uwtorzenie 4 zmiennnych
+
+    greet(name = "Damian", greeting = "Hi") //można stricte określac nazwe zmiennej i wartośc, nie trzeba wtedy podawac w kolejności deklaracji
 }
+
+fun greet(name: String, greeting: String = "Hello"): String { //deklaracja funkcji, można przypisywac wartości domyślne gdy trafi null
+    return "$greeting, $name!"
+}
+
+fun multiplyNumbers(a: Int, b: Int): Int = a * b // skrócony zapis dla funkcji z pojedyńa operacją, można w tym przypadku równiez pominąć typ zwracany
+
+// Kiedy funkcja nie zwraca nic, zwraca typ Unit podobnie jak Void w Java
