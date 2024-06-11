13566cee24bed44700ff39ebc3477114f241ccba
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-06-11 18:37:48 +0200

    null safety

--- src/main/kotlin/variables.kt
+++ src/main/kotlin/variables.kt
@@ -62,4 +62,24 @@ fun main() {
     val uInt: UInt = 5u; // typ unsigned (bez znaku -)
     val uInt2 = 5u; //lub bez typu
     val ulong = 5uL //specjalny suffix dla usigned longa
+
+    var name: String? = null //oznaczenie zmiennej jako nullable
+    var name2: String = null // to się nie skompiluje ponieważ zmienna nie została oznaczona jako nullable
+
+    //Handling nullables variables
+    name?.length //przez dodanie pytajnika wyciągamy bezpiecznie wartośc nawet jesli jest null do dostaniemy null
+    var length = name?.length ?: 0 // jeśli name jest null lub length by było null to zwrócimy 0
+    var length = name!!.length // rzuca wyjątek NullPointer jeśli name jest null, nalaży unikac stosowania
+    val x: Any = "Kotlin"
+    val y: String? = x as? String //bezpieczne rzutowanie, zwraca null jeśli x jest nullem
+    name?.let { //let keyword wykonuje metode jeśli nie jest nullem
+        println("Name is $it")
+    }
+
+    name?.apply { //wykonanie metody która ustawia wartośc name na inną jeśli jest nullem
+        // Configure object here
+    }
+
+    val positiveNumber = name.takeIf { it > 0 } // pobiera zmienną name jeśli spełnimy warunek, null jeśli nie spełnimy
+    val notBlankString = name.takeUnless { it.isBlank() } // pobiera zmienną name jeśli nie spełniamy warunku, null jesli spełnimy
 }
