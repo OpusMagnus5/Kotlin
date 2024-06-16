9e9908bb33380e38493e9371f67d1616d1dcacdc
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-06-16 16:23:04 +0200

    mutable lists

---
+++ src/main/kotlin/lists.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main () {
+    val southernCross = mutableListOf("Acrux", "Gacrux", "Imai", "Mimosa")
+    println(southernCross.joinToString()) //  Acrux, Gacrux, Imai, Mimosa
+
+    val stars = mutableListOf("Ginan", "Mu Crucis")
+    val newList = southernCross + stars //listy można łączyć dodając
+    val bo = southernCross == stars // można listy porównywac ich kontent i rozmiar
+    stars += "Damian" //można dodawać elementy w taki sposób
+
+    val toMutableList = newList.toMutableList() //tworzy kopie listy
+    mutableListOf(2, 3, 5).add()
+}
