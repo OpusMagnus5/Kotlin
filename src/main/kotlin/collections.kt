11e69987d1fc760d644b5a6f63d3c88c93bf00be
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-07-11 17:34:13 +0200

    maps

--- src/main/kotlin/collections.kt
+++ src/main/kotlin/collections.kt
@@ -17,4 +17,35 @@ fun main() {
     }
 
     for (element in 1..4 step 2) // co drugi element tylko z range
+        println(element)
+
+    for (element in stuff.indices) {} //iterowanie ale po indexach
+
+    for (index in stuff.indices.reversed()) {} //iterowanie w odrotnej kolejności
+
+    val students = mapOf( //tworzenie mapy
+        "Zhenya" to 5,
+        "Vlad" to 4,
+        "Nina" to 5
+    )
+
+    val values = mapOf<String, Int>("Second" to 2, "Third" to 3)
+    val map = buildMap<String, Int> { //inny sposób na inicjalizacje mapy
+        put("First", 1)
+        putAll(values)
+        put("Fourth", 4)
+    }
+
+    val grade = students["Nina"] //dostęp do wartości pod kluczem
+    val (name, grade1) = Pair("Zhenya", 5) //Pair jest typem dla entry w Mapie
+    val p = Pair(2, 3)
+    p.first // dostep do klucza
+    p.second // dostep do wartosci
+    val (name2, grade3) = "Vlad" to 4 // to samo co Pair
+
+    students.containsKey("")
+    students.containsValue(1) //sprawdzanie posiadania klucza i wartości
+
+    for ((key, value) in students) {} //iteracja
+    for (st in students) // durgi sposób
 }
