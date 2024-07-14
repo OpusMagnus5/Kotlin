78086551bcc17da5f599384c2315e160a0357d07
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-07-14 19:11:53 +0200

    constructors

--- src/main/kotlin/Clazz.kt
+++ src/main/kotlin/Clazz.kt
@@ -9,3 +9,23 @@ class Clazz {
 fun main() {
     val clazz = Clazz() // inicjalizacja klasy
 }
+
+class Person(val name: String, var age: Int) { //Primary konstruktor
+    init { //kod inicjalizujący wywoluje się po konstuktorze, można miec kilka takich bloków i wolują się w kolejnosci napisania
+        println("Helo Person")
+    }
+
+    constructor(name: String) : this(name, 0) { //secondary konstruktor musi wywołac primary jeśli istnieje
+        println("Secondary constructor called")
+    }
+}
+
+class Person2 {
+
+    val name: String
+    var age: Int
+    constructor(name: String, age: Int) { //secondary konstruktor
+        this.name = name
+        this.age = age
+    }
+}
