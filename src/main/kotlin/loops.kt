723b4262986578f9ae2f98fcaaa9dfb86dbc29c7
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-07-30 20:40:00 +0200

    loops and arrays

--- src/main/kotlin/loops.kt
+++ src/main/kotlin/loops.kt
@@ -1,6 +1,6 @@
 package org.example
 
-fun main() {
+fun example() {
     repeat(3) { // najprostrza pętla powtarzająca blok kodu określoną ilośc razy
         println("Hello")
     }
@@ -11,8 +11,25 @@ fun main() {
     }
 
     do { //while i do while tak samo jak w JAVA
-    } while (true)
+    } while (false)
 
-    while (true) {
+    while (false) {
     }
+
+    val stuff = emptyArray<Int>()
+
+    for (element in stuff) { //iterowanie
+        println(element)
+    }
+
+    for (element in 1..4 step 2) // co drugi element tylko z range
+        println(element)
+
+    for (element in stuff.indices) {} //iterowanie ale po indexach
+
+    for (index in stuff.indices.reversed()) {} //iterowanie w odrotnej kolejności
+
+    for (i in 0..stuff.lastIndex) {}
+
+    for (i in stuff.lastIndex downTo 0) {}
 }
