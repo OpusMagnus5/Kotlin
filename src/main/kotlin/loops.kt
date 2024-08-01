f0da0e6af9b85d5de324c2f06a55edcc6a5ed91a
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-08-01 18:50:48 +0200

    iterators

--- src/main/kotlin/loops.kt
+++ src/main/kotlin/loops.kt
@@ -1,6 +1,6 @@
 package org.example
 
-fun example() {
+fun example9() {
     repeat(3) { // najprostrza pętla powtarzająca blok kodu określoną ilośc razy
         println("Hello")
     }
@@ -32,4 +32,29 @@ fun example() {
     for (i in 0..stuff.lastIndex) {}
 
     for (i in stuff.lastIndex downTo 0) {}
+
+    // ITERATOR
+    var iterator = stuff.iterator()
+    while (iterator.hasNext()) {}
+    iterator.forEach {}
+
+    // Jeśli iterator jest z kolekcji mutable możemy usuwac jej elementy
+    val food = mutableSetOf("donuts", "cakes", "tarts")
+    val mutableIterator = food.iterator()
+    mutableIterator.remove()
+
+    // Listy mają specjalny operator ListIterator który pozwala na iterowanie w przód i tył
+    val strings = listOf("i", "like", "donuts")
+    val listIterator = strings.listIterator()
+    while (listIterator.hasPrevious()) {
+        listIterator.previous()
+        listIterator.previousIndex()
+    }
+
+    // Mutable listy mają swoją wersje ListIteratora
+    val words = mutableListOf("i", "know", "Claire")
+    val mutableListIterator = words.listIterator()
+    mutableListIterator.next()
+    mutableListIterator.set("don't know")
+    mutableListIterator.add("John")
 }
