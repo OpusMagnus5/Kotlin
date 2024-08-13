9bb06c0092895a97ac2ad3df96afab7988e09cdb
OpusMagnus5 <damianbodzioch95@gmail.com>
2024-08-13 11:10:13 +0200

    StringBuilder

--- src/main/kotlin/strings.kt
+++ src/main/kotlin/strings.kt
@@ -39,4 +39,27 @@ fun example10() {
     val charArray: CharArray = greeting.toCharArray()
     val arraysString = String(charArray)
 
+    /*
+    * StringBuilder reprezentujący zmienny ciąg znaków. Może być używany do wydajnego wykonywania wielu operacji
+    * manipulacji ciągami znaków, umożliwiając modyfikowanie jego zawartości bez tworzenia nowego obiektu.
+    * Klasa StringBuilder ma dwóch ważnych członków: length i capacity(). Właściwość length reprezentuje liczbę znaków zawartych w obiekcie StringBuilder,
+    * podczas gdy metoda capacity zwraca liczbę znaków, które obiekt może pomieścić, zanim konieczna będzie zmiana jego rozmiaru
+    */
+
+    val sb = StringBuilder() // z domyślną pojemnością początkową
+    val sb2 = StringBuilder(100) // określa początkową pojemność StringBuilder, przekazując konstruktorowi wartość całkowitą
+    val sb3 = StringBuilder("Kotlin")
+    val sb4 = StringBuilder(sb)
+    val str2 = buildString {
+        append("Hello") // umożliwia dodawanie znaków lub ciągów znaków na końcu konstruktora StringBuilder
+        insert(5, ",") // umożliwia wstawianie znaków lub ciągów znaków w określonej pozycji w konstruktorze StringBuilder
+        delete(5, 7) // metoda delete: umożliwia usuwanie znaków lub ciągów znaków z określonego zakresu w konstruktorze StringBuilder
+        replace(10, 13, "three") // Metoda replace: umożliwia zastępowanie znaków lub ciągów znaków w określonym zakresie w konstruktorze StringBuilder.
+        reverse() // Metoda reverse: umożliwia odwrócenie znaków w konstruktorze StringBuilder
+        append(2.0)
+    }
+    val str3 = buildString(100) {
+    }
+    sb.setLength(0) // resetuje stan buildera
+    sb.toString() // zwraca stringa
 }
